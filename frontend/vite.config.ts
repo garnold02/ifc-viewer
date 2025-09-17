b766afcede8622bdb9e0d84b7d4057fb176dacc5
garnold02 <gulyas.arnold02@protonmail.com>
2025-09-17 16:43:21 +0200

    chore: setup tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
 });
