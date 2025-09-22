e005751df9c43fefccec70ddec1c6d6c21aa20a5
garnold02 <gulyas.arnold02@protonmail.com>
2025-09-22 11:14:16 +0200

    chore: reorder imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
 import "./lib/i18next";
-import { queryClient } from "./lib/reactQuery";
-import { router } from "./lib/reactRouter";
-
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/reactQuery";
 import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./lib/reactRouter";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
