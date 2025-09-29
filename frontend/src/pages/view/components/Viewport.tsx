8df1185f475f25dead778ee72a736b7a9826dc9e
garnold02 <gulyas.arnold02@protonmail.com>
2025-09-29 22:15:52 +0200

    fix: only deselect when select tool is enabled

--- frontend/src/pages/view/components/Viewport.tsx
+++ frontend/src/pages/view/components/Viewport.tsx
@@ -5,8 +5,11 @@ import { IfcSceneLight } from "../../../components/IfcSceneLight";
 import { Scene } from "./Scene";
 import { useOutlinerStore } from "../../../stores/outliner/store";
 import { getCameraMoving } from "../../../global/camera";
+import { useToolStore } from "../../../stores/tool/store";
 
 export const Viewport = () => {
+  const toolContent = useToolStore((state) => state.content);
+
   const setSelectedNodeId = useOutlinerStore(
     (state) => state.setSelectedNodeId
   );
@@ -15,8 +18,10 @@ export const Viewport = () => {
     if (getCameraMoving()) {
       return;
     }
-    setSelectedNodeId(null);
-  }, [setSelectedNodeId]);
+    if (toolContent?.type === "select") {
+      setSelectedNodeId(null);
+    }
+  }, [setSelectedNodeId, toolContent?.type]);
 
   return (
     <IfcCanvas onClickCapture={onClick}>
