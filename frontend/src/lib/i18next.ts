05b7824e3102ce428bdf4b6f88e36e6756f39aa2
garnold02 <gulyas.arnold02@protonmail.com>
2025-10-01 23:16:31 +0200

    feature: remember preferred language

--- frontend/src/lib/i18next.ts
+++ frontend/src/lib/i18next.ts
@@ -2,6 +2,7 @@ import en_US from "../locales/en_US.json";
 import hu_HU from "../locales/hu_HU.json";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 declare module "i18next" {
   interface CustomTypeOptions {
@@ -13,12 +14,14 @@ declare module "i18next" {
   }
 }
 
-i18n.use(initReactI18next).init({
-  lng: "en_US",
-  fallbackLng: "en_US",
-  supportedLngs: ["en_US", "hu_HU"],
-  resources: {
-    en_US: { translation: en_US },
-    hu_HU: { translation: hu_HU },
-  },
-});
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en_US",
+    supportedLngs: ["en_US", "hu_HU"],
+    resources: {
+      en_US: { translation: en_US },
+      hu_HU: { translation: hu_HU },
+    },
+  });
