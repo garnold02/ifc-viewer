384e1731be972207907034ac2479df53389082e1
garnold02 <gulyas.arnold02@protonmail.com>
2025-10-01 16:13:38 +0200

    feature: navigate to home page when clicking on branding

--- frontend/src/components/Branding.tsx
+++ frontend/src/components/Branding.tsx
@@ -1,12 +1,15 @@
-import { Stack, Typography } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
 import { useTranslation } from "react-i18next";
+import { Stack, Typography } from "@mui/material";
+import { IconButtonLink } from "./IconButtonLink";
+import HomeIcon from "@mui/icons-material/Home";
 
 export const Branding = () => {
   const { t } = useTranslation(undefined, { keyPrefix: "components.Branding" });
   return (
     <Stack direction="row" alignItems="center" gap={1}>
-      <HomeIcon />
+      <IconButtonLink size="small" to="/">
+        <HomeIcon fontSize="small" />
+      </IconButtonLink>
       <Typography sx={{ userSelect: "none" }}>{t("title")}</Typography>
     </Stack>
   );
---
+++ frontend/src/components/IconButtonLink.tsx
@@ -0,0 +1,4 @@
+import { IconButton } from "@mui/material";
+import { createLink } from "@tanstack/react-router";
+
+export const IconButtonLink = createLink(IconButton);
